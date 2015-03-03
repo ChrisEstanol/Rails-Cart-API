d0e954c3a348d922abb8b06882e904ce3aa88e44
Christophe E <cestanol@gmail.com>
2015-03-03 12:31:49 -0500

    Change product title to name, add quantity and style views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  belongs_to :user
+
+  validates :name, :price, :quantity, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303155844) do
+ActiveRecord::Schema.define(version: 20150303171851) do
 
   create_table "products", force: :cascade do |t|
-    t.string   "title"
+    t.string   "name"
     t.text     "description"
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "quantity"
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"

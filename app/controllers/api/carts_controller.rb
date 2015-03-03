dc353945ebe1b95bcf64159952eaeec58dc28de5
Christophe E <cestanol@gmail.com>
2015-03-03 16:51:50 -0500

    API controller and resources

---
+++ app/controllers/api/carts_controller.rb
@@ -0,0 +1,38 @@
+class Api::CartsController < ApplicationController
+  # before_action :authenticate_user!
+  # Authenticate with token
+
+  def show
+    info = $redis.hgetall current_user_cart
+    @cart = Cart.new
+    info.each do |product_id, quantity|
+     @cart.add_item(CartItem.new(product_id, quantity))
+    end
+
+    respond_to do |format|
+      format.json { render json: @cart }
+    end
+  end
+
+  def add
+    product_id = params[:product_id].to_i
+    quantity = params[:quantity].to_i
+    $redis.hset current_user_cart, product_id, quantity
+
+    redirect_to product_path(product_id), notice: 'Product has been added to your cart.'
+  end
+
+  def remove
+    product_id = params[:product_id].to_i
+    $redis.hdel current_user_cart, product_id
+
+    redirect_to :back, notice: 'Product has been removed from your cart.'
+  end
+
+
+  private
+
+  def current_user_cart
+    "cart#{current_user.id}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'carts/show'
 
   resource :cart, only: [:show] do
     put 'add/:product_id', to: 'carts#add', as: :add_to
     put 'remove/:product_id', to: 'carts#remove', as: :remove_from
+  end
+
+  namespace :api do
+    resource :cart, only: [:show]
+    resources :products
   end
 
   devise_for :users

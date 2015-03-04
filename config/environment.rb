145c248976a7656f2c31a3b7653292358a91da0d
Christophe E <cestanol@gmail.com>
2015-03-04 10:18:01 -0500

    Config JBuilder to render CamelCase

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Convert keynames from the standard ruby_format to camelCase.
+Jbuilder.key_format camelize: :lower
+

88e98d0a70b4873b24e2bbb94a8d3055bf1093f2
Christophe E <cestanol@gmail.com>
2015-03-03 23:15:31 -0500

    Testing login and create products with Capybara

---
+++ spec/features/user_create_product_spec.rb
@@ -0,0 +1,16 @@
+# require 'spec_helper'
+
+# feature "User creates product" do
+#   scenario "successfully" do
+
+#     sign_in
+#     click_on "New Product"
+#     fill_in "Name", with: "Shoes"
+#     fill_in "Description", with: "Great shoes"
+#     fill_in "Quantity", with: 3
+#     fill_in "Price", with: 30
+#     click_on "Create"
+
+#     expect(page).to have_css 'p', text: "Shoes"
+#   end
+# end
